e261687fd699198cee38486c2b23b65c8d6f3e0d
ericjuma <ericjuma@gmail.com>
2020-07-09 01:36:27 -0400

    Add a bunch of chords, less thumb work

--- gergoplex/keymaps/mine/keymap.c
+++ gergoplex/keymaps/mine/keymap.c
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 [BASE] = LAYOUT_gergoplex(
     KC_Q,                KC_W, KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I,    KC_O,    KC_P, 
-    MT(MOD_LCTL, KC_A),  KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,    MT(MOD_LCTL, KC_SCLN),
-    MT(MOD_RSFT, KC_Z),  KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT,  MT(MOD_RSFT, KC_SLSH),
+    MT(MOD_LCTL, KC_A),  KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,    MT(MOD_LGUI, KC_SCLN),
+    MT(MOD_LGUI, KC_Z),  KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT,  MT(MOD_LCTL, KC_SLSH),
 
-    LT(SYMB, KC_ESC), MT(MOD_RSFT, KC_ENT), ALT_T(KC_BSPC),
-    KC_ENT, MT(MOD_LGUI, KC_SPC), LT(NUMB, KC_TAB)
+    LT(SYMB, KC_NO), MT(MOD_RSFT, KC_ENT), ALT_T(KC_BSPC),
+    KC_TAB, MT(NUMB, KC_SPC), LT(NUMB, KC_NO)
     ),
     /* Keymap 1: Symbols layer
      * ,-----------------------------.       ,--------------------------------.
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
 [NUMB] = LAYOUT_gergoplex(
-    KC_5,  KC_6,  KC_7,  KC_8,     KC_9,       KC_NO,    KC_6,    KC_7,     KC_8,    KC_9,
-    KC_0,  KC_1,  KC_2,  KC_3,     KC_4,       KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT, KC_VOLU,
-    KC_5,  KC_6,  KC_7,  KC_8,     KC_9,       KC_MS_L,  KC_MS_D, KC_MS_U,  KC_MS_R, KC_VOLD,
-                  KC_NO, KC_BTN1,  KC_BTN2,    KC_BTN2,  KC_BTN1, KC_MNXT
+    KC_5,  KC_LCBR,  KC_LPRN,  KC_LBRC,     KC_9,       KC_NO,  KC_LBRC,   KC_LPRN,  KC_LCBR,   KC_9,
+    KC_1,  KC_2,  KC_3,  KC_4,     KC_5,     KC_6,    KC_7,     KC_8,    KC_9,    KC_0,
+    KC_5,  KC_6,  KC_7,  KC_BTN1,     KC_BTN2,       KC_MS_L,  KC_MS_D, KC_MS_U,  KC_MS_R, KC_VOLD,
+                  KC_NO, KC_TILD,  KC_BTN2,    KC_BTN2,  KC_BTN1, KC_MNXT
     )
 };
